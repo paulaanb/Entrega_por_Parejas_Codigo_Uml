13e4f770832ef689d7e5e640c3d0352f1af4fffc
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-21 23:12:35 +0100

    Agregamos el enunciado para saber lo que nos pide el programa

--- Clases/Palindromo_metodo_de_clase.py
+++ Clases/Palindromo_metodo_de_clase.py
@@ -1,3 +1,4 @@
+#Enunciado: crear una clase Palindromo que contenga un método de clase esPalindromo(), que devuelve un valor booleano que indica si una cadena de caracteres pasada como argumento es un palíndromo. Un palíndromo es una cadena que se puede leer de izquierda a derecha o de derecha a izquierda. Se tienen en cuenta los caracteres no alfanuméricos.
 frase = str(input('Introduce la frase: '))
 sum = 0
 max = 0
