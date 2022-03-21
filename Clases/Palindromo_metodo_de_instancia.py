7afdbe01d9859a2fe344942f78d67a03a85d2d9d
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-21 23:13:06 +0100

    Agregamos el enunciado tambien

--- Clases/Palindromo_metodo_de_instancia.py
+++ Clases/Palindromo_metodo_de_instancia.py
@@ -1,3 +1,4 @@
+#Enunciado: en esta misma clase Palindromo, añada un atributo que se inicializará en el constructor. Añada también un método test() que pruebe si el atributo de la instancia es un palíndromo. Además, al destruir la instancia, muestre el atributo en mayúsculas.
 class palindromo():
   def __init__(self,palabra):
     self.palabra=palabra
